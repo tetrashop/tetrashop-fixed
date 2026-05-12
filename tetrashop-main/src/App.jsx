0d5dc601bfbd1e4a484e753b5145deda5a890d87
Your Name <your@email.com>
2026-05-12 16:13:58 +0330

    Remove tetrashop-main folder, keep only README.md at root

--- tetrashop-main/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ServicePlatform } from './components/ServicePlatform';
-import './index.css';
-function App() {
-  return <div className="App"><ServicePlatform /></div>;
-}
-export default App;
